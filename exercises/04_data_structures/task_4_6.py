e9d717bc357d2446ae8e8ca32789f9c47ebf5429
mityagz <mityagz@gmail.com>
2020-12-31 13:18:38 -0600

    4.6

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -14,3 +14,6 @@ Outbound Interface    FastEthernet0/0
 """
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+
+l0 = ospf_route.replace(",", "").split()
+print("Prefix{:>35}\nAD/Metric{:>35}\nNext-Hop{:>25}\nLast update{:>25}\nOutbound Interface{:>25}".format(l0[0], l0[1], l0[3], l0[4], l0[5]))
