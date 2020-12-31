56d0842c981bf357aedcbb20b24e03d943ba572f
mityagz <mityagz@gmail.com>
2020-12-31 06:21:30 -0600

    s4.1

--- exercises/04_data_structures/task_4_1.py
+++ exercises/04_data_structures/task_4_1.py
@@ -10,3 +10,7 @@
 """
 
 nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+
+s0 = nat.replace("Fast","Gigabit")
+print(s0)
+#Out[170]: 'ip nat inside source list ACL interface GigabitEthernet0/1 overload'
