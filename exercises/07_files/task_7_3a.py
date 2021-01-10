704afbc35cf1648fc88980db949e7eeb111a2cad
mityagz <mityagz@gmail.com>
2021-01-10 10:48:06 -0600

    7.3

--- exercises/07_files/task_7_3a.py
+++ exercises/07_files/task_7_3a.py
@@ -24,3 +24,35 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+import sys
+
+cam = {}
+keys = []
+
+with open(sys.argv[1], 'r') as f_cam:
+    for l in f_cam:
+        if l.find('DYNAMIC') != -1:
+            al0 = l.split()
+            cam_k = int(al0[0])
+            if cam.get(cam_k):
+                cam[cam_k].append([al0[1], al0[3]])
+            else:
+                cam[cam_k] = []
+                cam[cam_k].append([al0[1], al0[3]])
+
+
+for k in cam:
+    keys.append(k)
+
+keys.sort()
+
+vid = input('Enter vlan or vlan list:')
+vid_list = vid.split(',')
+
+for k in keys:
+    for l in cam[k]:
+        for vid_l in vid_list:
+            if int(vid_l) == k:
+                #print(k, l[0], l[1], cam[k])
+                print(k, l[0], l[1])
