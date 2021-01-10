84c301d584e8f91328eed12b7e795d4f7747195a
mityagz <mityagz@gmail.com>
2021-01-10 01:36:22 -0600

    7.2c

--- exercises/07_files/task_7_2b.py
+++ exercises/07_files/task_7_2b.py
@@ -14,3 +14,29 @@
 """
 
 ignore = ["duplex", "alias", "Current configuration"]
+
+import sys
+
+out_list = []
+
+with open(sys.argv[1], 'r') as f:
+  with open('config_sw1_cleared.txt', 'w') as fo:
+    for l in f:
+        ar = l.strip()
+        for w in ignore:
+            if ar.startswith(w):
+                break
+        else:
+            out_list.append(ar)
+    # It is the first variant, writes list
+    #fo.write('\n'.join(out_list))
+            # the second variant write by string + '\n'
+            #fo.write(ar +  '\n')
+            # the third variant with writelines()
+            #fo.writelines(ar + '\n')
+    # the four variant
+    out_list2 = []
+    for x in out_list:
+        out_list2.append(x + '\n')
+    fo.writelines(out_list2)
+
