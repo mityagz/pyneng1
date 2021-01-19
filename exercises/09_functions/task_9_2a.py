e72d05d810e7aa7636a4b65486d6fca87388dbf7
mityagz <mityagz@gmail.com>
2021-01-19 08:06:45 -0600

    9.2a

--- exercises/09_functions/task_9_2a.py
+++ exercises/09_functions/task_9_2a.py
@@ -26,3 +26,27 @@ trunk_config = {
     "FastEthernet0/2": [11, 30],
     "FastEthernet0/4": [17],
 }
+
+def generate_trunk_config(intf_vlan_mapping, trunk_template):
+    """
+    {'FastEthernet0/1': [10, 20],
+     'FastEthernet0/2': [11, 30],
+     'FastEthernet0/4': [17]}
+    trunk_template - список команд для порта в режиме access
+
+    Возвращает список всех портов в режиме trunk с конфигурацией на основе шаблона
+    """
+    r = {}
+    for intf, vids in intf_vlan_mapping.items():
+        i = intf
+        r[i] = []
+        for t in trunk_template:
+            if t.startswith('switchport trunk allowed vlan'):
+                r[i].append(t + ' {}'.format(','.join([ str(v) for v in vids ])))
+            else:
+                r[i].append(t)
+    return(r)
+
+print(generate_trunk_config(trunk_config, trunk_mode_template))
+print()
+#print(generate_trunk_config(trunk_config_2, trunk_mode_template))
