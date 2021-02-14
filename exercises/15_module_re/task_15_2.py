3d3ec1c14ac37e8bcebc78a7cea96805e043b08d
mityagz <mityagz@gmail.com>
2021-02-14 04:12:29 -0600

    15.2

--- exercises/15_module_re/task_15_2.py
+++ exercises/15_module_re/task_15_2.py
@@ -20,4 +20,30 @@
 
 Проверить работу функции на примере файла sh_ip_int_br.txt.
 
+Interface                  IP-Address      OK? Method Status                Protocol
+FastEthernet0/0            15.0.15.1       YES manual up                    up
+FastEthernet0/1            10.0.12.1       YES manual up                    up
+FastEthernet0/2            10.0.13.1       YES manual up                    up
+FastEthernet0/3            unassigned      YES unset  administratively down down
+Loopback0                  10.1.1.1        YES manual up                    up
+Loopback100                100.0.0.1       YES manual up                    up
+
+
 """
+import re
+
+def parse_sh_ip_int_br(fname):
+    result = []
+    with open(fname, 'r') as fd:
+        conf = fd.read()
+        regexp = (r'(?P<intf>\S+)\s+(?P<ip>(\d{1,3}\.){3}\d{1,3}|unassigned)\s+\w+\s+\w+\s+(?P<status>up|down|administratively down)\s+(?P<proto>up|down)')
+        iter = re.finditer(regexp, conf)
+        for i in iter:
+            result.append(i.group('intf', 'ip', 'status', 'proto'))
+    return result
+
+
+
+
+if __name__ == '__main__':
+    print(parse_sh_ip_int_br('sh_ip_int_br.txt'))
