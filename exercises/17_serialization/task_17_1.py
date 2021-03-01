5ce2bf5b7e3bbb18d709e3a10069bdc8a478d4c0
mityagz <mityagz@gmail.com>
2021-03-01 03:05:07 -0600

    17.1

--- exercises/17_serialization/task_17_1.py
+++ exercises/17_serialization/task_17_1.py
@@ -28,3 +28,31 @@ sw3,00:E9:22:11:A6:50,100.1.1.7,3,FastEthernet0/21
 Первый столбец в csv файле имя коммутатора надо получить из имени файла, остальные - из содержимого в файлах.
 
 """
+
+import re
+import csv
+
+def write_dhcp_snooping_to_csv(filenames, output):
+    res = []
+    for f in filenames:
+        hostname = f.split('_')[0]
+        with open(f, 'r') as ff:
+            for line in ff:
+                match = re.search(r'(?P<mac>(((\d|\w){2}:){5})(\d|\w){2})\s+'
+                                  r'(?P<ip>((\d{1,3})\.){3}(\d{1,3}))\s+'
+                                  r'\d+\s+dhcp-snooping\s+(?P<vid>\d+)\s+'
+                                  r'(?P<intf>\S+)', line)
+                if(match):
+                    r = list(match.group('mac', 'ip', 'vid', 'intf'))
+                    r.insert(0, hostname)
+                    res.append(r)
+
+    res.insert(0, [ 'switch' , 'mac', 'ip', 'vlan', 'interface' ])
+    print(res)
+    with open(output, 'w') as fout:
+       writer = csv.writer(fout)
+       for row in res:
+            writer.writerow(row)
+
+if __name__ == '__main__':
+    write_dhcp_snooping_to_csv([ 'sw1_dhcp_snooping.txt', 'sw2_dhcp_snooping.txt', 'sw3_dhcp_snooping.txt' ], 'dhcp.csv')
