90a6119fb7b9db2b764190510b3bdfe6447c9798
mityagz <mityagz@gmail.com>
2021-03-03 06:15:56 -0600

    17.3a

--- exercises/17_serialization/task_17_3.py
+++ exercises/17_serialization/task_17_3.py
@@ -14,6 +14,7 @@ R4>show cdp neighbors
 Device ID    Local Intrfce   Holdtme     Capability       Platform    Port ID
 R5           Fa 0/1          122           R S I           2811       Fa 0/1
 R6           Fa 0/2          143           R S I           2811       Fa 0/0
+SW1              Eth 0/0            131          S I      WS-C3750- Eth 0/3
 
 Функция должна вернуть такой словарь:
 {'R4': {'Fa 0/1': {'R5': 'Fa 0/1'},
@@ -30,7 +31,7 @@ import re
 def parse_sh_cdp_neighbors(sh_cdp_nei):
     res = {}
     m0 = re.search(r'(?P<lhost>\S+)>show cdp neighbors', sh_cdp_nei)
-    m1 = re.finditer(r'(?P<rhost>\S+)\s+(?P<lintf>\w+ \S+)\s+\d+.*\d+\s+(?P<rintf>\w+ \S+)', sh_cdp_nei)
+    m1 = re.finditer(r'(?P<rhost>\S+)\s+(?P<lintf>\w+ \S+)\s+\d+.*[\d+|\S+]\s+(?P<rintf>\w+ \S+)', sh_cdp_nei)
     if m0 and m1:
         lhost = m0.group('lhost')
         for m in m1:
