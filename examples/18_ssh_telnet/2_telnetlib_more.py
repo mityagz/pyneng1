eda2158e650956b84361227bf413950af6646fb4
mityagz <mityagz@gmail.com>
2021-03-20 06:56:21 -0500

    /2_telnetlib_more.py

--- examples/18_ssh_telnet/2_telnetlib_more.py
+++ examples/18_ssh_telnet/2_telnetlib_more.py
@@ -7,19 +7,23 @@ import re
 def to_bytes(line):
     return f"{line}\n".encode("utf-8")
 
+def pmpt(line):
+    return ("\w+@\w+" + line).encode("utf-8")
 
-def send_show_command(ip, username, password, enable, command):
+def send_show_command(ip, username, password, enable, command, prompt='>'):
     with telnetlib.Telnet(ip) as telnet:
-        telnet.read_until(b"Username")
+        telnet.read_until(b"login:")
         telnet.write(to_bytes(username))
-        telnet.read_until(b"Password")
+        telnet.read_until(b"Password:")
         telnet.write(to_bytes(password))
-        index, m, output = telnet.expect([b">", b"#"])
+        index, m, output = telnet.expect([ pmpt(prompt) ])
+        """
         if index == 0:
             telnet.write(b"enable\n")
             telnet.read_until(b"Password")
             telnet.write(to_bytes(enable))
             telnet.read_until(b"#", timeout=5)
+        """
         time.sleep(3)
         telnet.read_very_eager()
 
@@ -27,9 +31,9 @@ def send_show_command(ip, username, password, enable, command):
         result = ""
 
         while True:
-            index, match, output = telnet.expect([b"--More--", b"#"], timeout=5)
+            index, match, output = telnet.expect([ b"\-{3}\(more( \d{1,3}\%)?\)\-{3}", pmpt(prompt) ], timeout=5)
             output = output.decode("utf-8")
-            output = re.sub(" +--More--| +\x08+ +\x08+", "\n", output)
+            #output = re.sub(" +--more--| +\x08+ +\x08+", "\n", output)
             result += output
             if index in (1, -1):
                 break
@@ -41,7 +45,8 @@ def send_show_command(ip, username, password, enable, command):
 
 
 if __name__ == "__main__":
-    devices = ["192.168.100.1", "192.168.100.2", "192.168.100.3"]
+    devices = [ "10.248.0.65", "10.248.0.66" ]
     for ip in devices:
-        result = send_show_command(ip, "cisco", "cisco", "cisco", "sh run")
-        pprint(result, width=120)
+        result = send_show_command(ip, "am", "qwerty", "cisco", "show conf")
+        #pprint(result, width=120)
+        print(result)
