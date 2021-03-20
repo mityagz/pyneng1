531e8bc9a1df3f9cec274c561052e0c7bb914fee
mityagz <mityagz@gmail.com>
2021-03-20 04:48:39 -0500

    2_telnetlib.py

--- examples/18_ssh_telnet/2_telnetlib.py
+++ examples/18_ssh_telnet/2_telnetlib.py
@@ -6,35 +6,42 @@ from pprint import pprint
 def to_bytes(line):
     return f"{line}\n".encode("utf-8")
 
+def fpmpt(prompt):
+    pmpt = ("\w+@\w+" + prompt).encode('utf-8')
+    return pmpt
 
-def send_show_command(ip, username, password, enable, commands):
+
+def send_show_command(ip, username, password, enable, commands, prompt=">"):
+    pmpt = fpmpt(prompt)
     with telnetlib.Telnet(ip) as telnet:
-        telnet.read_until(b"Username")
+        telnet.read_until(b"login:")
         telnet.write(to_bytes(username))
-        telnet.read_until(b"Password")
+        telnet.read_until(b"Password:")
         telnet.write(to_bytes(password))
-        index, m, output = telnet.expect([b">", b"#"])
+        index, m, output = telnet.expect([ pmpt ])
+        """
         if index == 0:
             telnet.write(b"enable\n")
             telnet.read_until(b"Password")
             telnet.write(to_bytes(enable))
             telnet.read_until(b"#", timeout=5)
-        telnet.write(b"terminal length 0\n")
-        telnet.read_until(b"#", timeout=5)
+        """
+        telnet.write(b"set cli screen-length 0\n")
+        telnet.read_until(pmpt, timeout=5)
         time.sleep(3)
         telnet.read_very_eager()
 
         result = {}
         for command in commands:
             telnet.write(to_bytes(command))
-            output = telnet.read_until(b"#", timeout=5).decode("utf-8")
+            output = telnet.read_until(pmpt, timeout=5).decode("utf-8")
             result[command] = output.replace("\r\n", "\n")
         return result
 
 
 if __name__ == "__main__":
-    devices = ["192.168.100.1", "192.168.100.2", "192.168.100.3"]
-    commands = ["sh ip int br", "sh arp"]
+    devices = [ "10.248.0.65" ]
+    commands = ["show interface brief", "show arp"]
     for ip in devices:
-        result = send_show_command(ip, "cisco", "cisco", "cisco", commands)
+        result = send_show_command(ip, "am", "qwerty", "cisco", commands)
         pprint(result, width=120)
