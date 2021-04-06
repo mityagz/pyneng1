d8f9a5cae5e42e4c04b11381a3a1f2d45217dd5d
mityagz <mityagz@gmail.com>
2021-04-06 12:37:06 -0500

    1a, 1b

--- exercises/18_ssh_telnet/task_18_1a.py
+++ exercises/18_ssh_telnet/task_18_1a.py
@@ -10,3 +10,30 @@
 
 Для проверки измените пароль на устройстве или в файле devices.yaml.
 """
+
+import yaml
+from netmiko import ConnectHandler
+from paramiko.ssh_exception import AuthenticationException
+#from paramiko.ssh_exception import TimeoutException
+
+def send_show_command(device, command):
+    result = {}
+    try:
+        with ConnectHandler(**device) as ssh:
+            output = ssh.send_command(command)
+            return output
+    except (AuthenticationException) as auth_error:
+        print(auth_error)
+        print("Authentication on {} is fail".format(device['host']))
+    #except (TimeoutException) as timeout_error:
+        #print(error)
+        #print("Connection to {} is fail, timeout raise".format(device))
+
+
+if __name__ == "__main__":
+    command = "show interfaces brief"
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+
+    for dev in devices:
+        print(send_show_command(dev, command))
--- exercises/18_ssh_telnet/task_18_1b.py
+++ exercises/18_ssh_telnet/task_18_1b.py
@@ -11,3 +11,33 @@
 
 Для проверки измените IP-адрес на устройстве или в файле devices.yaml.
 """
+
+import yaml
+from netmiko import ConnectHandler
+from paramiko.ssh_exception import AuthenticationException
+from netmiko.ssh_exception import NetmikoTimeoutException
+
+def send_show_command(device, command):
+    result = {}
+    try:
+        with ConnectHandler(**device) as ssh:
+            output = ssh.send_command(command)
+            return output
+    except (AuthenticationException) as auth_error:
+        print(auth_error)
+        print("Authentication on {} is fail".format(device['host']))
+    except (NetmikoTimeoutException) as timeout_error:
+        print(timeout_error)
+        print("Connection to {} is fail, timeout raise".format(device['host']))
+    except (IOError) as io_err:
+        print(io_err)
+        print("IOError to {} appeared".format(device['host']))
+
+
+if __name__ == "__main__":
+    command = "show interfaces brief"
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+
+    for dev in devices:
+        print(send_show_command(dev, command))
