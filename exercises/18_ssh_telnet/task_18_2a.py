0edc3f66cf3c91bc7f6cb47ac245d8e5aa1ea676
mityagz <mityagz@gmail.com>
2021-04-13 01:44:36 -0500

    2a

--- exercises/18_ssh_telnet/task_18_2a.py
+++ exercises/18_ssh_telnet/task_18_2a.py
@@ -19,3 +19,37 @@ In [15]:
 
 Скрипт должен отправлять список команд commands на все устройства из файла devices.yaml с помощью функции send_config_commands.
 """
+
+commands = ["set system login user cisco1 authentication encrypted-password \"$1$ABzJUPdE$NvSj.G1FVSjPrcZURlgwF0\"", "set system login user cisco1 class super-user"]
+
+import yaml
+from netmiko import ConnectHandler
+from paramiko.ssh_exception import AuthenticationException
+from netmiko.ssh_exception import NetmikoTimeoutException
+
+def send_config_commands(device, config_commands, log = True):
+    result = {}
+    try:
+        with ConnectHandler(**device) as ssh:
+            if log:
+                print("Подключаюсь к {}".format(device['host']))
+            output = ssh.config_mode()
+            output += ssh.send_config_set(config_commands)
+            ssh.commit()
+            return output
+    except (AuthenticationException) as auth_error:
+        print(auth_error)
+        print("Authentication on {} is fail".format(device['host']))
+    except (NetmikoTimeoutException) as timeout_error:
+        print(timeout_error)
+        print("Connection to {} is fail, timeout raise".format(device['host']))
+    except (IOError) as io_err:
+        print(io_err)
+        print("IOError to {} appeared".format(device['host']))
+
+if __name__ == "__main__":
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+
+    for dev in devices:
+        print(send_config_commands(dev, commands))
