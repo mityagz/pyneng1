4dc9ea699dc2ab1d7bda4e7f8a82194206622e7e
mityagz <mityagz@gmail.com>
2021-04-30 06:16:29 -0500

    18.2b

--- exercises/18_ssh_telnet/task_18_2b.py
+++ exercises/18_ssh_telnet/task_18_2b.py
@@ -91,10 +91,72 @@ R1(config)#logging
 
 R1(config)#a
 % Ambiguous command:  "a"
+
+am@CE1> show interfacu    
+             ^
+syntax error, expecting <command>.
+am@CE1> show interfaces yy 
+error: device yy not found
+
+am@CE1> a
+        ^
+unknown command.
+am@CE1> 
+
 """
 
+
 # списки команд с ошибками и без:
-commands_with_errors = ["logging 0255.255.1", "logging", "a"]
-correct_commands = ["logging buffered 20010", "ip http server"]
+#commands_with_errors = ["logging 0255.255.1", "logging", "a"]
+#correct_commands = ["logging buffered 20010", "ip http server"]
+commands_with_errors = ["run show interfacu", "run show interfaces yy", "a"]
+correct_commands = [ "set system login user cisco1 authentication encrypted-password \"$1$ABzJUPdE$NvSj.G1FVSjPrcZURlgwF0\"", "set system login user cisco1 class super-user" ]
 
 commands = commands_with_errors + correct_commands
+
+
+import yaml
+from netmiko import ConnectHandler
+from paramiko.ssh_exception import AuthenticationException
+from netmiko.ssh_exception import NetmikoTimeoutException
+import re
+
+def send_config_commands(device, config_commands, log = True):
+    d0 = {}
+    d1 = {}
+    try:
+        with ConnectHandler(**device) as ssh:
+            if log:
+                print("Подключаюсь к {}".format(device['host']))
+            for command in config_commands:
+                output = ssh.config_mode()
+                output += ssh.send_config_set(command)
+                ssh.commit()
+                isError = False
+                for err in [ "syntax error, expecting <command>.", "error: device yy not found", "unknown command.", "is ambiguous.\nPossible completions:" ]:
+                    regex = None
+                    regex = re.search(err, output)
+                    if regex:
+                        isError = True
+                if isError:
+                    d1[command] = output
+                else:
+                    d0[command] = output
+                output = ''
+            return (d0, d1)
+    except (AuthenticationException) as auth_error:
+        print(auth_error)
+        print("Authentication on {} is fail".format(device['host']))
+    except (NetmikoTimeoutException) as timeout_error:
+        print(timeout_error)
+        print("Connection to {} is fail, timeout raise".format(device['host']))
+    except (IOError) as io_err:
+        print(io_err)
+        print("IOError to {} appeared".format(device['host']))
+
+if __name__ == "__main__":
+    with open("devices.yaml") as f:
+        devices = yaml.safe_load(f)
+
+    for dev in devices:
+        print(send_config_commands(dev, commands))
