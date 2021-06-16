26c2e7a854742f90ec895abc744ca178685c1836
mityagz <mityagz@gmail.com>
2021-06-16 04:44:09 -0500

    change source of command

--- examples/19_concurrent_connections/netmiko_threads_map_exceptions.py
+++ examples/19_concurrent_connections/netmiko_threads_map_exceptions.py
@@ -19,6 +19,7 @@ logging.basicConfig(
 def send_show(device_dict, command):
     start_msg = '===> {} Connection: {}'
     received_msg = '<=== {} Received:   {}'
+    device_dict.pop('command', None)
     ip = device_dict['ip']
     logging.info(start_msg.format(datetime.now().time(), ip))
     if ip == '192.168.100.1': time.sleep(5)
@@ -36,7 +37,9 @@ def send_show(device_dict, command):
 def send_command_to_devices(devices, command):
     data = {}
     with ThreadPoolExecutor(max_workers=2) as executor:
-        result = executor.map(send_show, devices, repeat(command))
+        #result = executor.map(send_show, devices, repeat(command))
+        pprint([ dev['command'] for dev in devices ])
+        result = executor.map(send_show, devices, [ dev['command'] for dev in devices ])
         for device, output in zip(devices, result):
             data[device['ip']] = output
     return data
@@ -45,5 +48,8 @@ def send_command_to_devices(devices, command):
 if __name__ == '__main__':
     with open('devices.yaml') as f:
         devices = yaml.safe_load(f)
-    pprint(send_command_to_devices(devices, 'sh ip int br'))
+    #pprint(send_command_to_devices(devices, 'sh ip int br'))
+    #pprint(send_command_to_devices(devices[0:len(devices) - 1], 'show system uptime'))
+    #pprint(send_command_to_devices(devices[0:len(devices)], devices[len(devices)-1]['command']))
+    pprint(send_command_to_devices(devices[0:len(devices)], devices))
 
