ea058dd942d97f489ad4031e37f6ccaa278474ad
mityagz <mityagz@gmail.com>
2021-06-20 23:38:58 -0500

    19.2

--- exercises/19_concurrent_connections/task_19_2.py
+++ exercises/19_concurrent_connections/task_19_2.py
@@ -33,3 +33,71 @@ Ethernet0/1                unassigned      YES NVRAM  administratively down down
 
 Проверить работу функции на устройствах из файла devices.yaml
 """
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from pprint import pprint
+from datetime import datetime
+import time
+from itertools import repeat
+import logging
+
+import yaml
+from netmiko import ConnectHandler
+
+from paramiko.ssh_exception import AuthenticationException
+#from netmiko.ssh_exception import NetmikoTimeoutException
+
+logging.getLogger("paramiko").setLevel(logging.WARNING)
+
+logging.basicConfig(
+    format = '%(threadName)s %(name)s %(levelname)s: %(message)s',
+    level=logging.INFO)
+
+start_msg = '===> {} Connection: {}'
+received_msg = '<=== {} Received: {}'
+
+
+def send_show(device_dict, command):
+    ip = device_dict['ip']
+    logging.info(start_msg.format(datetime.now().time(), ip))
+    with ConnectHandler(**device_dict) as ssh:
+        prompt = ssh.find_prompt()
+        ssh.write_channel(f"{command}\n")
+        output = ""
+        while True:
+            try:
+                page = ssh.read_until_pattern(f"More|{prompt}")
+                output += page
+                if "More" in page:
+                    ssh.write_channel(" ")
+                elif prompt in output:
+                    break
+            except NetmikoTimeoutException:
+                break
+        logging.info(received_msg.format(datetime.now().time(), ip))
+        return {ip: output}
+
+
+def send_show_command_to_devices(devices, command, filename, limit=3):
+    data = {}
+    log = []
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        future_ssh = [ executor.submit(send_show, device, command) for device in devices ]
+        for f in as_completed(future_ssh):
+            try:
+                result = f.result()
+            except NetMikoAuthenticationException as e:
+                print(e)
+            else:
+                data.update(result)
+    
+    for dev, entry in data.items():
+        log.append("{} : {}".format(dev, entry))
+    with open(filename, 'w') as f:
+        f.writelines(log)
+    return data
+
+
+if __name__ == '__main__':
+    with open('devices0.yaml') as f:
+        devices = yaml.safe_load(f)
+    pprint(send_show_command_to_devices(devices, "show  system uptime", "uptime.log", 4))
