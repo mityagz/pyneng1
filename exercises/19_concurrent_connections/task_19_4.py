6221474faf83b2ae741b93c5fe45a53435e3a9d1
mityagz <mityagz@gmail.com>
2021-06-21 19:50:30 -0500

    19.4

--- exercises/19_concurrent_connections/task_19_4.py
+++ exercises/19_concurrent_connections/task_19_4.py
@@ -86,3 +86,126 @@ R3#
 
 Для выполнения задания можно создавать любые дополнительные функции.
 """
+
+# Этот словарь нужен только для проверки работа кода, в нем можно менять IP-адреса
+# тест берет адреса из файла devices.yaml
+commands = {
+    "10.240.0.65": [ "show configuration protocols ospf", "show configuration protocols lldp" ],
+    "10.240.0.66": [ "show interfaces brief", "show configuration protocols lldp" ],
+    "10.248.0.65": [ "show interfaces descriptions" ],
+    "10.248.0.66": [ "show interfaces descriptions", "show configuration protocols lldp" ],
+}
+
+commands_config = [ "set system login user cisco1 authentication encrypted-password \"$1$ABzJUPdE$NvSj.G1FVSjPrcZURlgwF0\"", "set system login user cisco1 class super-user" ]
+
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from pprint import pprint
+from datetime import datetime
+import time
+from itertools import repeat
+import logging
+
+import yaml
+from netmiko import ConnectHandler
+
+
+from paramiko.ssh_exception import AuthenticationException
+from netmiko.ssh_exception import NetmikoTimeoutException, NetMikoAuthenticationException
+
+logging.getLogger("paramiko").setLevel(logging.WARNING)
+
+logging.basicConfig(
+    format = '%(threadName)s %(name)s %(levelname)s: %(message)s',
+    level=logging.INFO)
+
+start_msg = '===> {} Connection: {}'
+received_msg = '<=== {} Received: {}'
+
+def send_show_commands(device_dict, command):
+    ip = device_dict['ip']
+    print(ip, command)
+    logging.info(start_msg.format(datetime.now().time(), ip))
+    with ConnectHandler(**device_dict) as ssh:
+        prompt = ssh.find_prompt()
+        ssh.write_channel(f"{command}\n")
+        output = ""
+        while True:
+            try:
+                page = ssh.read_until_pattern(f"More|{prompt}")
+                output += page
+                if "More" in page:
+                    ssh.write_channel(" ")
+                elif prompt in output:
+                    break
+            except NetmikoTimeoutException:
+                    break
+    logging.info(received_msg.format(datetime.now().time(), ip))
+    return {ip: output}
+
+
+def send_config_commands(device, config_commands):
+    result = {}
+    ip = device['ip']
+    logging.info(start_msg.format(datetime.now().time(), ip))
+    try:
+        with ConnectHandler(**device) as ssh:
+            output = ssh.config_mode()
+            output += ssh.send_config_set(config_commands)
+            ssh.commit()
+            logging.info(received_msg.format(datetime.now().time(), ip))
+            return {ip: output}
+    except (AuthenticationException) as auth_error:
+        print(auth_error)
+        print("Authentication on {} is fail".format(device['host']))
+    except (NetmikoTimeoutException) as timeout_error:
+        print(timeout_error)
+        print("Connection to {} is fail, timeout raise".format(device['host']))
+    except (IOError) as io_err:
+        print(io_err)
+        print("IOError to {} appeared".format(device['host']))
+        return {ip: output}
+
+
+def send_commands_to_devices(devices, filename, limit = 3, *, show = None, config = None):
+    data = {}
+    data_e = {}
+    log = []
+    futures_ssh = []
+    with ThreadPoolExecutor(max_workers=limit) as executor:
+        if show != None and config != None:
+            raise ValueError()
+        elif show != None:
+            print([ "{} {}".format(device['ip'], c) for device in devices for c in commands[device['ip']]])
+            futures_ssh = [ executor.submit(send_show_commands, device, c) for device in devices for c in commands[device['ip']] ]
+        else:
+            print([ "{} {}".format(device['ip'], c) for device in devices for c in commands_config ])
+            futures_ssh = [ executor.submit(send_config_commands, device, config) for device in devices ]
+
+        for f in as_completed(futures_ssh):
+            try:
+                result = f.result()
+            except NetMikoAuthenticationException as e:
+                print(e)
+            else:
+                ip_dev = ((list(result.keys()))[0])
+                out_val = ((list(result.values()))[0])
+                if data_e.get(ip_dev) == None:
+                    data_e[ip_dev] = []
+                    data_e[ip_dev].append(out_val)
+                else:
+                    data_e[ip_dev].append(out_val)
+
+    for dev, entries in data_e.items():
+        for entry in data_e[dev]:
+            log.append("{} : {}".format(dev, entry))
+    with open(filename, 'w') as f:
+        f.writelines(log)
+    return data_e
+
+
+if __name__ == '__main__':
+    with open('devices0.yaml') as f:
+        devices = yaml.safe_load(f)
+        #send_commands_to_devices(devices, "commands_result.log", 4, show = commands)
+        send_commands_to_devices(devices, "commands_result.log", 4, config = commands_config)
