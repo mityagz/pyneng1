b717fe7c0148b684538b1248ffe4f2269c0b3dc3
mityagz <mityagz@gmail.com>
2021-07-01 12:25:21 +0500

    21.2

--- exercises/21_textfsm/task_21_2.py
+++ exercises/21_textfsm/task_21_2.py
@@ -14,3 +14,46 @@
 
 Проверить работу шаблона с помощью функции parse_command_output из задания 21.1.
 """
+from netmiko import ConnectHandler
+import textfsm
+
+def do_parse(tmpl, show_out):
+    with open(tmpl) as t:
+        re_table = textfsm.TextFSM(t)
+        header = re_table.header
+        res = re_table.ParseText(show_out)
+        res.insert(0, header)
+    return res
+
+def parse_command_output(tmpl, show_out):
+    return (do_parse(tmpl, show_out))
+
+
+# вызов функции должен выглядеть так
+if __name__ == "__main__":
+    r1_params = {
+        "device_type": "cisco_ios",
+        "host": "192.168.100.1",
+        "username": "cisco",
+        "password": "cisco",
+        "secret": "cisco",
+    }
+    #with ConnectHandler(**r1_params) as r1:
+        #r1.enable()
+        #output = r1.send_command("sh ip int br")
+    with open("output/sh_ip_dhcp_snooping.txt") as f:
+     output = f.read()
+    result = parse_command_output("templates/sh_ip_dhcp_snooping.template", output)
+    print(result)
+
+"""
+#00:09:BB:3D:D6:58   10.1.10.2        86250       dhcp-snooping   10    FastEthernet0/1
+Value mac (([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2}))
+Value ip (((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))
+Value vlan (\d{1,4})
+Value intf (\S+)
+
+Start
+  ^${mac}\s+${ip}\s+\d+\s+\S+\s+${vlan}\s+${intf} -> Record
+
+"""
