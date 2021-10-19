b605726b048496243872a73cf07bff763e0cd162
mityagz <mityagz@gmail.com>
2021-10-19 15:38:39 +0500

    1a

--- exercises/22_oop_basics/task_22_1a.py
+++ exercises/22_oop_basics/task_22_1a.py
@@ -10,9 +10,6 @@
 """
 
 
-class Topology:
-    def __init__(self, topology_dict):
-        self.topology = self._normalize(topology_dict)
 
 
 topology_example = {
@@ -26,3 +23,28 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+    
+    def _normalize(self, topo):
+        t = {}
+        for i in topo.keys():
+            if i == topo.get(topo.get(i)):
+                if t.get(i) == None and t.get(topo.get(i)) == None:
+                    t[i] = topo[i]
+            else:
+                    t[i] = topo[i]
+        return t
+
+    def gettopo(self):
+        return self.topology
+
+def main():
+    top = Topology(topology_example)
+    for t in (top.gettopo()):
+        print(t, (top.gettopo())[t])
+
+if __name__ == "__main__":
+   main()
