4fce385ab868cb7d84a5eff44c69fd48dc5c86a7
mityagz <mityagz@gmail.com>
2021-10-19 18:22:29 +0500

    1b

--- exercises/22_oop_basics/task_22_1b.py
+++ exercises/22_oop_basics/task_22_1b.py
@@ -63,3 +63,38 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+    
+    def _normalize(self, topo):
+        t = {}
+        for i in topo.keys():
+            if i == topo.get(topo.get(i)):
+                if t.get(i) == None and t.get(topo.get(i)) == None:
+                    t[i] = topo[i]
+            else:
+                    t[i] = topo[i]
+        return t
+
+    def gettopo(self):
+        return self.topology
+
+    def delete_link(self, a, b):
+        if self.topology.get(a) == b:
+            self.topology.pop(a)
+            return
+        if self.topology.get(b) == a:
+            self.topology.pop(b)
+            return
+        print("Такого соединения нет")
+
+def main():
+    top = Topology(topology_example)
+    top.delete_link(("SW1", "Eth0/2"), ("R2", "Eth0/0"))
+    for t in (top.gettopo()):
+        print(t, (top.gettopo())[t])
+
+if __name__ == "__main__":
+   main()
