8abfef90e5d9b9c5985e9ece71f12815e3c42cb0
mityagz <mityagz@gmail.com>
2021-10-20 09:50:09 +0500

    1c

--- exercises/22_oop_basics/task_22_1c.py
+++ exercises/22_oop_basics/task_22_1c.py
@@ -47,3 +47,59 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+    
+    def _normalize(self, topo):
+        t = {}
+        for i in topo.keys():
+            if i == topo.get(topo.get(i)):
+                if t.get(i) == None and t.get(topo.get(i)) == None:
+                    t[i] = topo[i]
+            else:
+                    t[i] = topo[i]
+        return t
+
+    def gettopo(self):
+        return self.topology
+
+    def delete_link(self, a, b):
+        if self.topology.get(a) == b:
+            self.topology.pop(a)
+            return
+        if self.topology.get(b) == a:
+            self.topology.pop(b)
+            return
+        print("Такого соединения нет")
+
+    def delete_node(self, n):
+        list_to_del = []
+        for x, y in self.topology.items():
+            ha = x[0]
+            hb = y[0]
+            if ha == n:
+                list_to_del.append(x)
+            if hb == n:
+                list_to_del.append(x)
+        #print(list_to_del)
+        if len(list_to_del) == 0:
+            print('Такого устройства нет')
+            return
+        else:
+            for x in (list_to_del):
+                self.topology.pop(x)
+
+def main():
+    top = Topology(topology_example)
+    top.delete_link(("SW1", "Eth0/2"), ("R2", "Eth0/0"))
+    for t in (top.gettopo()):
+        print(t, (top.gettopo())[t])
+    print("-----------------")
+    top.delete_node('SW1')
+    top.delete_node('R4')
+    print(top.gettopo())
+
+if __name__ == "__main__":
+   main()
