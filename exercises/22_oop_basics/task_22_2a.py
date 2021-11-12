81479431355f13476dc9365dcdfa36939c3f4dc7
mityagz <mityagz@gmail.com>
2021-11-12 15:02:45 +0500

    22_2a

--- exercises/22_oop_basics/task_22_2a.py
+++ exercises/22_oop_basics/task_22_2a.py
@@ -43,3 +43,70 @@ Out[5]: 'sh ip int br | exclude unassigned\r\nInterface                  IP-Addr
 
 
 """
+
+
+import telnetlib
+import time
+from pprint import pprint
+import yaml
+import sys
+import textfsm
+from tabulate import tabulate
+from textfsm import clitable
+
+pe8_p = {
+    'ip': '10.229.10.0',
+    'username': 'am',
+    'password': 'qwerty',
+    'secret': 'qwerty'}
+
+class CiscoTelnet:
+    def __init__(self, ip, username, password, secret):
+            self.telnet = telnetlib.Telnet(ip)
+            self.telnet.read_until(b"Username")
+
+            self._write_line(username)
+            self.telnet.read_until(b"Password")
+            self._write_line(password)
+            index, m, output = self.telnet.expect([b">", b"#"])
+            if index == 0:
+                #self.telnet.write(b"enable\n")
+                self._write_line("enable")
+                self.telnet.read_until(b"Password")
+                self._write_line(secret)
+                self.telnet.read_until(b"#", timeout=5)
+                self._write_line("terminal length 0")
+                self.telnet.read_until(b"#", timeout=5)
+                time.sleep(3)
+                self.telnet.read_very_eager()
+    
+    def _write_line(self, line):
+        return self.telnet.write(line.encode("ascii") + b"\n")
+
+    def send_show_command(self, str_cmd, parse = False, templates = 'templates', index = 'index'):
+        r = {}
+        self._write_line(str_cmd)
+        if not parse:
+            output = self.telnet.read_until(b"#", timeout=5).decode("utf-8")
+            return output
+        else:
+            result = []
+            cli_table = clitable.CliTable('index', 'templates')
+            attributes = {'Command': 'show ip interface brief', 'Vendor': 'Cisco'}
+            output = self.telnet.read_until(b"#", timeout=5).decode("utf-8")
+            cli_table.ParseCmd(output, attributes)
+            result = [ dict(zip(cli_table.header, v)) for v in [ list(row) for row in cli_table ] ]
+            """
+            for ll in data_rows:
+                d = {}
+                for idx, v in enumerate(ll):
+                        d[cli_table.header[idx]] = v
+                result.append(d)
+            """
+            #result = [ dict(zip(cli_table.header, v)) for v in data_rows ]
+            return result
+
+if __name__ == '__main__':
+    c = CiscoTelnet(**pe8_p)
+    r = c.send_show_command("sh ip int br", parse = True)
+    pprint(r, width=120)
