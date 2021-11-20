fab0de35ce3096622272f7a6494e68dbfd73b79a
mityagz <mityagz@gmail.com>
2021-11-20 20:57:54 +0500

    23_3a

--- exercises/23_oop_special_methods/task_23_3a.py
+++ exercises/23_oop_special_methods/task_23_3a.py
@@ -40,3 +40,88 @@ topology_example = {
     ("SW1", "Eth0/2"): ("R2", "Eth0/0"),
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
+
+topology_example2 = {
+    ("R1", "Eth0/4"): ("R7", "Eth0/0"),
+    ("R1", "Eth0/6"): ("R9", "Eth0/0"),
+}
+
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+        self.t0 = topology_dict
+
+    def __iter__(self):
+        return iter([ (k, v) for k, v in self.topology.items() ])
+    
+    def _normalize(self, topo):
+        t = {}
+        for i in topo.keys():
+            if i == topo.get(topo.get(i)):
+                if t.get(i) == None and t.get(topo.get(i)) == None:
+                    t[i] = topo[i]
+            else:
+                    t[i] = topo[i]
+        return t
+    
+    def _get_topo(self):
+        return self.t0
+
+    def __add__(self, tp1):
+        t2 = self.t0.copy()
+        t1 = tp1._get_topo()
+        for k in t1:
+            t2[k] = t1[k]
+        return Topology(t2)
+
+
+    def gettopo(self):
+        return self.topology
+
+    def delete_link(self, a, b):
+        if self.topology.get(a) == b:
+            self.topology.pop(a)
+            return
+        if self.topology.get(b) == a:
+            self.topology.pop(b)
+            return
+        print("Такого соединения нет")
+
+    def delete_node(self, n):
+        list_to_del = []
+        for x, y in self.topology.items():
+            ha = x[0]
+            hb = y[0]
+            if ha == n:
+                list_to_del.append(x)
+            if hb == n:
+                list_to_del.append(x)
+        #print(list_to_del)
+        if len(list_to_del) == 0:
+            print('Такого устройства нет')
+            return
+        else:
+            for x in (list_to_del):
+                self.topology.pop(x)
+
+def main():
+    t1 = Topology(topology_example)
+    t2 = Topology(topology_example2)
+    t3 = t1 + t2
+    print(t3.gettopo())
+    top = Topology(topology_example)
+    for link in top:
+        pass
+
+    """
+    top.delete_link(("SW1", "Eth0/2"), ("R2", "Eth0/0"))
+    for t in (top.gettopo()):
+        print(t, (top.gettopo())[t])
+    print("-----------------")
+    top.delete_node('SW1')
+    top.delete_node('R4')
+    print(top.gettopo())
+    """
+if __name__ == "__main__":
+   main()
