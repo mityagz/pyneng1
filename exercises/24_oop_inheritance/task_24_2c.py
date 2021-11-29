0ae0bd78e66ee01e8f1504d5fb304f0282145619
mityagz <mityagz@gmail.com>
2021-11-29 07:49:24 +0500

    2c

--- exercises/24_oop_inheritance/task_24_2c.py
+++ exercises/24_oop_inheritance/task_24_2c.py
@@ -19,3 +19,56 @@ In [5]: r1.send_command('sh ip int br', strip_command=True)
 Out[5]: 'Interface                  IP-Address      OK? Method Status                Protocol\nEthernet0/0                192.168.100.1   YES NVRAM  up                    up      \nEthernet0/1                192.168.200.1   YES NVRAM  up                    up      \nEthernet0/2                190.16.200.1    YES NVRAM  up                    up      \nEthernet0/3                192.168.230.1   YES NVRAM  up                    up      \nEthernet0/3.100            10.100.0.1      YES NVRAM  up                    up      \nEthernet0/3.200            10.200.0.1      YES NVRAM  up                    up      \nEthernet0/3.300            10.30.0.1       YES NVRAM  up                    up      '
 
 """
+
+class ErrorInCommand(Exception):
+    """
+    Исключение генерируется, если при выполнении команды на оборудовании, возникла ошибка.
+    """
+
+from netmiko.cisco.cisco_ios import CiscoIosSSH
+import re
+
+
+device_params = {
+    "device_type": "cisco_ios",
+    "ip": "10.229.10.0",
+    "username": "am",
+    "password": "qwerty",
+    "secret": "cisco",
+}
+
+
+
+class MyNetmiko(CiscoIosSSH):
+    #def __init__(self, device_type, ip, username, password, secret, disable_paging = True):
+    def __init__(self, **device_params):
+        self.dev_ip = device_params['ip']
+        super().__init__(**device_params)
+        self.enable()
+
+
+    def _check_error_in_command(self, cmd, ocmd):
+            regex = None
+            err = r'% (?P<err>.*)'
+            regex = re.search(err, ocmd)
+            if regex:
+                excn = "При выполнении команды \"{}\" на устройстве {} возникла ошибка {}".format(cmd, self.dev_ip, regex.group('err'))
+                raise ErrorInCommand(excn)
+
+    def send_command(self, *args, **kwargs):
+        cmd = args
+        ocmd = super().send_command(*args, **kwargs)
+        if cmd == 'logging':
+            ocmd = super().send_config_set(cmd)
+        self._check_error_in_command(*args, ocmd)
+        return ocmd
+
+    def send_config_set(self, cmd):
+        ocmd = super().send_config_set(cmd)
+        self._check_error_in_command(cmd, ocmd)
+        return ocmd
+
+if __name__ == '__main__':
+    r = MyNetmiko(**device_params)
+    print(r.send_command('sh ip int br'))
+    print(r.send_command('sh ip br'))
